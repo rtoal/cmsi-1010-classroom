c3e75362e416f8ddc1637ab6fcb71031d4378f8e
Ray Toal <rtoal@lmu.edu>
2025-06-02 13:15:38 -0700

    Test cases for functions cardio

--- pset_starters/pset_01/functions_cardio.py
+++ pset_starters/pset_01/functions_cardio.py
@@ -80,3 +80,47 @@ def word_frequencies(s):
     """
     # replace the pass statement with your code
     pass
+
+
+print(is_odd(3), " should be True")
+print(is_odd(8), " should be False")
+print(is_odd(-3), " should be True")
+print(is_odd(-8), " should be False")
+
+print(median_of_three(1, 2, 3), " should be 2")
+print(median_of_three(1, 3, 2), " should be 2")
+print(median_of_three(2, 1, 3), " should be 2")
+print(median_of_three(2, 3, 1), " should be 2")
+print(median_of_three(3, 1, 2), " should be 2")
+print(median_of_three(3, 2, 1), " should be 2")
+
+print(factorial(5), " should be 120")
+print(factorial(0), " should be 1")
+print(factorial(1), " should be 1")
+print(factorial(6), " should be 720")
+print(factorial(20), " should be 2432902008176640000")
+
+print(is_palindrome("racecar"), " should be True")
+print(is_palindrome("hello"), " should be False")
+print(is_palindrome("madam"), " should be True")
+print(is_palindrome("python"), " should be False")
+
+print(count_of_latin_vowels("hello world"), " should be 3")
+print(count_of_latin_vowels("aeiou"), " should be 5")
+print(count_of_latin_vowels("xyz"), " should be 0")
+print(count_of_latin_vowels("Python programming"), " should be 4")
+print(count_of_latin_vowels("Aeiou"), " should be 5")
+
+print(longest_string(["apple", "banana", "cherry"]), " should be banana")
+print(longest_string(["cat", "dog", "elephant"]), " should be elephant")
+print(longest_string(["short", "longer", "longest"]), " should be longest")
+print(longest_string(["a", "ab", "abc"]), " should be abc")
+print(longest_string(["one", "two", "three", "four"]), " should be three")
+
+print(word_frequencies("hello world hello"),
+      " should be {'hello': 2, 'world': 1}")
+print(word_frequencies("Python is great Python"),
+      " should be {'Python': 2, 'is': 1, 'great': 1}")
+print(word_frequencies("a b a c b a"), " should be {'a': 3, 'b': 2, 'c': 1}")
+print(word_frequencies("test test test"), " should be {'test': 3}")
+print(word_frequencies(""), " should be {}")

f751b660c3da02f86156a945ac3f7fb979fa8ba8
Ray Toal <rtoal@lmu.edu>
2025-06-06 16:45:25 -0700

    Show calls to one of the interest functions

--- labs/interest.py
+++ labs/interest.py
@@ -22,3 +22,9 @@ print(investment_value(1000, 0.05, 0, 0, 10))  # should be 1628.89
 print(investment_value(1000, 0.05, 0, 100, 10))  # should be 2886.68
 print(investment_value(10000, 0.13, 0.25, 1000, 30))  # should be 319883.75
 print(investment_value(1, 1, 0, 0, 20))  # should be 1048576.0
+
+print(years_to_reach_goal(1000, 0.05, 0, 0, 2000))  # should be 15
+print(years_to_reach_goal(1000, 0.05, 0, 100, 2000))  # should be 6
+print(years_to_reach_goal(1000, 0.05, 0, 100, 5000))  # should be 18
+print(years_to_reach_goal(10000, 0.13, 0.25, 1000, 100000))  # should be 19
+print(years_to_reach_goal(1, 1, 0, 0, 1000000))  # should be 20

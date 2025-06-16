4ebf9c2a1023e016575b56c0fa07211bb5eab224
Ray Toal <rtoal@lmu.edu>
2025-06-15 20:49:21 -0700

    tree program

--- labs/tree.py
+++ labs/tree.py
@@ -1,25 +1,22 @@
-from math import pi, radians
+from math import radians
 from Tree.core import Tree
 from PIL import Image
 
+background_color = (200, 240, 250)
+leaf_color = (30, 160, 30)
+trunk_width = 25
+base_trunk_color = (50, 40, 0)
+small_branch_color = (180, 130, 30)
+branch_gradient = (*base_trunk_color, *small_branch_color)
 
-def main():
-    tree = Tree(
-        pos=(0, 0, 0, -500),
-        branches=(
-            (.5, radians(-30)),
-            (.6, radians(30)),
-            (.4, radians(60))))
+trunk_length = 200
+first_branch_line = (0, 0, 0, -trunk_length)
+scales_and_angles = [(0.75, radians(-30)), (0.75, radians(30))]
+age = 10
 
-    # Let the tree grow
-    tree.grow(10)
-
-    # Make sure the tree fits in the image
-    tree.move_in_rectangle()
-
-    im = Image.new("RGB", tree.get_size(), (239, 239, 239))
-    tree.draw_on(im, (85, 25, 0, 128, 53, 21), (0, 62, 21), 10)
-    im.show()
-
-
-main()
+tree = Tree(pos=first_branch_line, branches=scales_and_angles)
+tree.grow(age)
+tree.move_in_rectangle()
+image = Image.new("RGB", tree.get_size(), background_color)
+tree.draw_on(image, branch_gradient, leaf_color, trunk_width)
+image.show()

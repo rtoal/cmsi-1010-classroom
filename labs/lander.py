3320a792a1437c1a0aa5c5b2cbfd3b5e6128f0f9
Ray Toal <rtoal@lmu.edu>
2025-06-18 09:53:39 -0700

    Plane lander improvements

--- labs/lander.py
+++ labs/lander.py
@@ -8,12 +8,16 @@ from dataclasses import dataclass
 import pygame
 
 pygame.init()
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1024, 600
 GROUND_LEVEL = HEIGHT - 100  # y-coordinate of the ground
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Plane Landing")
 
 tree_spacing = 160
+stripe_start, stripe_length = 0, 100
+STRIPE_LEVEL = HEIGHT - 50  # y-coordinate of the stripes
+
+MAX_PLANE_SPEED = 1
 
 
 @dataclass
@@ -23,24 +27,32 @@ class Plane:
     width: int
     height: int
     state: str = "flying"  # "flying", "descending", "ground"
-    speed: int = 5
+    speed: int = 8
 
     def draw(self):
         color = (128, 128, 128)
-        pygame.draw.ellipse(
-            screen, color, (WIDTH//2-self.width, self.y, self.width, self.height))
+        # pygame.draw.ellipse(
+        #     screen, color, (WIDTH//2-self.width, self.y, self.width, self.height))
+        x, y = WIDTH // 2 - self.width, self.y
+        plane_relatives = [
+            (0, 0), (3, 2), (1, 7), (4, 7), (8, 2), (15, 2), (10, 6), (11, 6),
+            (24, 2), (32, 2), (35, -2), (24, -2), (11, -8), (10, -8), (15, -2),
+            (3, -2)
+        ]
+        plane = [(WIDTH//2 + 4*x, self.y - 4*y) for x, y in plane_relatives]
+        pygame.draw.polygon(screen, (128, 128, 128), plane)
 
     def move(self):
         if self.state != "stopped":
             self.x += self.speed % (tree_spacing * 20)
         if self.state == "descending":
             self.y += self.speed * 0.1
-            if self.y >= GROUND_LEVEL:
+            if self.y >= STRIPE_LEVEL:
                 self.state = "ground"
-                self.y = GROUND_LEVEL
+                self.y = STRIPE_LEVEL
         elif self.state == "ground":
-            self.y = HEIGHT - 50
-            self.speed -= 0.01
+            self.y = STRIPE_LEVEL
+            self.speed -= 0.005
             if self.speed <= 0:
                 self.speed = 0
                 self.state = "stopped"
@@ -49,28 +61,23 @@ class Plane:
 plane = Plane(0, y=50, width=100, height=30)
 
 
-def draw_trees():
-    x = tree_spacing - (plane.x % WIDTH)
-    while x < WIDTH:
-        pygame.draw.rect(screen, (139, 69, 19),
-                         (x, GROUND_LEVEL - 50, 20, 50))  # trunk
-        pygame.draw.polygon(screen, (0, 128, 0), [(x - 30, GROUND_LEVEL - 50),
-                                                  (x + 10, GROUND_LEVEL - 100),
-                                                  # leaves
-                                                  (x + 50, GROUND_LEVEL - 50)])
-        pygame.draw.polygon(screen, (0, 128, 0), [(x - 20, GROUND_LEVEL - 70),
-                                                  (x + 10, GROUND_LEVEL - 120),
-                                                  # leaves
-                                                  (x + 40, GROUND_LEVEL - 70)])
-        x += tree_spacing
-
-
 def draw_scene():
+    global stripe_start
     screen.fill((0, 255, 255))  # clear screen
     pygame.draw.rect(screen, (0, 128, 0), (0, HEIGHT - 100, WIDTH, 100))
-    draw_trees()
+    pygame.draw.rect(screen, (0, 0, 0), (0, HEIGHT-70, WIDTH, 50))
+    x = stripe_start
+    while x < WIDTH:
+        pygame.draw.line(screen, (255, 255, 255), (x, HEIGHT - 50),
+                         (x + stripe_length, HEIGHT - 50), 5)
+        x += (stripe_length * 1.5)
+    # draw_trees()
     plane.draw()
     plane.move()
+    stripe_start -= plane.speed
+    if stripe_start*1.5 < -stripe_length:
+        stripe_start += stripe_length*1.5
+
     pygame.display.flip()
 
 
@@ -91,5 +98,5 @@ while True:
                     plane.state = "flying"
                     plane.x = 0
                     plane.y = 50
-                    plane.speed = 5
+                    plane.speed = MAX_PLANE_SPEED
     draw_scene()
